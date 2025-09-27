b84f3ddbf85d60211360d75697ef15c42789bf1b
Maaz Ansari <maazansari25667@gmail.com>
2025-09-27 16:57:25 +0530

    fix(starfield): resolve tsparticles v2/v3 version conflict causing 'drawer.validTypes is not iterable' error

--- components/Starfield.tsx
+++ components/Starfield.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useMemo } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "tsparticles-slim";
+import { loadSlim } from "@tsparticles/slim";
 import { useEffect, useState } from "react";
 
 export default function Starfield() {
